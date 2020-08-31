8952702ba969e01cac5c07524f4383b43a588bb7
sibendudey <sibendu.dey@gmail.com>
2020-08-30 21:22:40 -0500

    Base url IP

--- othello-app/src/BaseUrl.js
+++ othello-app/src/BaseUrl.js
@@ -1,7 +1,8 @@
-const HOST_ADDRESS = '35.153.31.251';
-const PORT = '8092';
+const HOST_ADDRESS = '34.201.10.173';
+const PORT = '8443';
 const HTTP_PROTOCOL = 'http://';
-export const BASE_URL = HTTP_PROTOCOL + HOST_ADDRESS + ':' + PORT;
+const HTTPS_PROTOCOL = 'https://';
+export const BASE_URL = HTTPS_PROTOCOL + HOST_ADDRESS + ':' + PORT;
 
 const SOCKET_PORT = '9090';
 const SOCKET_PROTOCOL = 'ws://';
